d24b07381ccc2c4fda7765ed95fa394cfd48cc55
Caio Lima Albuquerque <caiolima2802@gmail.com>
2025-10-21 19:34:33 -0300

    Tava faltando o rótulo e o ret, agora está completo o primeiro exercício

--- lab11/c1_1.s
+++ lab11/c1_1.s
@@ -2,10 +2,11 @@
     my_var: .word 10
 
 .text
-.globl increment_my_var
+.globl increment_my_var, my_var
 
 increment_my_var:
     la t0, my_var   # Carrega o endereço da variável global
     lw t1, 0(t0)    # Carrega o valor da variável
     addi t1, t1, 1  # Adiciona 1 nesse valor
     sw t1, 0(t0)    # Salva novamente no endereço dela
+    ret
---
+++ lab11/c1_5.s
@@ -0,0 +1,4 @@
+.text
+.globl operation
+
+operation:
