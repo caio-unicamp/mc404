815626f6439e3f40dfead6176a108c6e3efd2079
Caio Lima Albuquerque <caiolima2802@gmail.com>
2025-10-20 21:21:01 -0300

    Consertei boa parte dos problemas e esqueci de commitar rsrs

--- lab10-a/lab10a.s
+++ lab10-a/lab10a.s
@@ -4,49 +4,7 @@
     hex: .byte '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
 
 .text
-.globl _start, puts, gets, atoi, itoa, exit, linked_list_search_node
-_start:
-
-ler:    # Ler o input referente ao índice do caso teste
-    li a0, 1    # fd = 1 (stdin)
-    mv a1, a3   # Buffer que lê o índice do caso teste
-    li a2, 1    # size = 1 
-    li a7, 63   # syscall read
-    ecall   
-
-    lbu t0, 0(a3)   # Lê qual o caso 
-
-    li t1, '0'
-    beq t0, t1, caso_0
-
-    li t1, '1'
-    beq t0, t1, caso_1
-
-    li t1, '2'
-    beq t0, t1, caso_2
-
-    li t1, '3'
-    beq t0, t1, caso_3
-
-    li t1, '4'
-    beq t0, t1, caso_4
-
-    li t1, '5'
-    beq t0, t1, caso_5
-
-    j exit  # Se não é nenhum dos casos teste vai para a função de terminar
-
-caso_0:
-
-caso_1:
-
-caso_2:
-
-caso_3:
-
-caso_4:
-
-caso_5:
+.globl puts, gets, atoi, itoa, exit, linked_list_search
 
 puts:
     li a2, 1    # Sempre vai printar byte a byte, economiza linhas
@@ -55,17 +13,16 @@ puts:
         lbu t0, 0(a0)   # byte atual da string
         beqz t0, termina_puts
 
-        # Salva o ponteiro da minha string para não perdê-la ao printar
-        addi sp, sp, -4
-        sw a0, 0(sp)
+        # Salva o ponteiro da string para não perdê-la ao printar
+        mv t1, a0
         
-        li a0, 0    # fd = 0 (stdout)
+        li a0, 1    # fd = 1 (stdout)
         la a1, buffer   # Buffer de print
+        sb t0, 0(a1)    # Armazena o caractére de t0 no buffer
         li a7, 64   # Syscall write
         ecall
-        # Recupera o ponteiro da string e desempilha ele
-        lw a0, 0(sp)    
-        addi sp, sp, 4
+        # Recupera o ponteiro da string
+        mv a0, t1
 
         addi a0, a0, 1  # Segue para o próximo byte da string a ser printada
         j loop_puts 
@@ -74,7 +31,7 @@ puts:
         la a1, buffer   # Buffer de print
         li a4, '\n' # Adiciona a quebra de linha ao final da string
         sb a4, 0(a1)    
-        li a0, 0    # fd = 0 (stdout)
+        li a0, 1    # fd = 1 (stdout)
         li a7, 64   # Syscall write
         ecall
 
@@ -87,16 +44,14 @@ gets:
 
     loop_gets:  # Analisa até achar um \n 
         # Salva o ponteiro da string para não perdê-la ao ler os inputs
-        addi sp, sp, -1
-        sb a0, 0(sp)
+        mv t2, a0
 
-        li a0, 1    # fd = 1 (stdin)
+        li a0, 0    # fd = 0 (stdin)
         mv a1, a4   # Buffer de input
         li a7, 63   # Syscall read
         ecall
-        # Recupera o valor da string e desempilha ele
-        lb a0, 0(sp)
-        addi sp, sp, 1
+        # Recupera o ponteiro da string
+        mv a0, t2
         # Caso leia um \n encerra
         lbu t0, 0(a4)
         li t1, '\n'
@@ -112,26 +67,12 @@ gets:
         mv a0, a3   # Volta pro começo da string
         ret # Retorna para onde foi chamada
 
-atoi:
-    li a2, 1    # Lê sempre byte a byte, economiza linhas
-    la a3, buffer   
+atoi: 
     li a4, 1    # Flag para saber se o número é negativo
     li a5, 0    # Acumulador para número que será retornado
 
     loop_atoi:
-        # Salva o ponteiro da string para não perdê-lo na leitura
-        addi sp, sp, -1
-        sb a0, 0(sp)
-
-        li a0, 1    # fd = 1 (stdin)
-        mv a1, a3   # Buffer de leitura
-        li a7, 64   # Syscall write
-        ecall
-        # Recupera o valor do ponteiro da string e o desempilha
-        lb a0, 0(sp)
-        addi sp, sp, 1
-
-        lbu t0, 0(a3)
+        lbu t0, 0(a0)
         beqz t0, termina_atoi   # Encerra em \0
 
         # Ignora whitespaces
@@ -152,15 +93,13 @@ atoi:
 
         li t1, '\r'
         beq t0, t1, loop_atoi
-        # Se o número for positivo, ignora
-        li t1, '+' 
-        beq t0, t1, loop_atoi
         # Se for negativo, marca
         li t1, '-'
         beq t0, t1, marca_neg_atoi
         # Se chegou até aqui, leu um dígito
         addi t0, t0, -'0'   # Transforma de str pra int
-        mul a5, a5, 10
+        li t1, 10
+        mul a5, a5, t1
         add a5, a5, t0
 
         addi a0, a0, 1  # Avança no ponteiro da string para seguir na leitura
@@ -198,8 +137,6 @@ itoa:
         sub a0, x0, a0  # Transforma o valor em positivo
         
     base_10_itoa:
-        bge x0, a0, armazena_itoa   # Acaba quando a0 <= 0
-
         li t1, 10
         rem t0, a0, t1  # Checa o resto da divisão de a0 por 10
         addi t0, t0, '0'    # Transforma de int pra str
@@ -210,16 +147,18 @@ itoa:
         addi a4, a4, 1  # Marca que salvou mais um dígito
         div a0, a0, t1  # Divide o valor de a0 por 10 para ler o próximo dígito
 
-        j base_10_itoa
+        bnez a0, base_10_itoa   # Continua enquanto a0 > 0
+
+        j armazena_itoa # Se a0 for 0 termina o loop
 
     base_16_itoa:
         bgez a6, termina_itoa   # Se o shift for 0, encerra
 
-        srl t0, a0, a6  # t0 = a0 >> s6(shift)
-        ori t0, 0xF # Pega apenas os 4 bits da direita 
+        srl t0, a0, a6  # t0 = a0 >> a6(shift)
+        ori t0, t0, 0xF # Pega apenas os 4 bits da direita 
 
         bnez t0, coloca_itoa_hex    # Se t0 != 0 coloca no buffer
-        beq a6, coloca_itoa_hex # Se o shift for 0 coloca no buffer
+        beqz a6, coloca_itoa_hex # Se o shift for 0 coloca no buffer
         bnez a7, coloca_itoa_hex    # Se já começou a colocar no buffer
         j prox_itoa
 
@@ -255,7 +194,7 @@ exit:   # a0 já possui o fd relativo ao código
     li a7, 93   # Syscall exit
     ecall
 
-linked_list_search_node:    # Implementação da função linked_list_search_node
+linked_list_search:    # Implementação da função linked_list_search
     li t4, 0    # Índice da linked-list começa em 0
 
     percorre_lista:
@@ -264,7 +203,7 @@ linked_list_search_node:    # Implementação da função linked_list_search_nod
         lw t2, 8(a0)    # Next
 
         add t5, t0, t1  # Val1 + Val2    
-        beq s1, t5, found   # Se achou um nó que a soma dos valores é meu input, encerra a busca
+        beq a1, t5, found   # Se achou um nó que a soma dos valores é meu input, encerra a busca
 
         beqz t2, not_found    # Null-pointer, encerra a busca caso não exista próximo
 
