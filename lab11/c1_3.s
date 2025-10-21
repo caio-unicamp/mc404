f53e0a49276156ffa835de66ce91a3fb28e0a270
Caio Lima Albuquerque <caiolima2802@gmail.com>
2025-10-21 00:58:54 -0300

    Agora sim, faltava alguns detalhes

--- lab11/c1_3.s
+++ lab11/c1_3.s
@@ -20,73 +20,66 @@
 operation:
     # Armazena int a = 1
     la t0, a
-    lw t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    lw a0, 0(t0)
     # Armazena int b = -2
     la t0, b
-    lw t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    lw a1, 0(t0)
     # Armazena short c = 3
-    la t0, a
-    lh t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    la t0, c
+    lh a2, 0(t0)
     # Armazena short d = -4
-    la t0, a
-    lh t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    la t0, d
+    lh a3, 0(t0)
     # Armazena char e = 5
-    la t0, a
-    lb t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    la t0, e
+    lb a4, 0(t0)
     # Armazena char f = -6
-    la t0, a
-    lb t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    la t0, f
+    lb a5, 0(t0)
     # Armazena int g = 7
-    la t0, a
-    lw t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    la t0, g
+    lw a6, 0(t0)
     # Armazena int h = -8
-    la t0, a
-    lw t1, 0(t0)
-    addi sp, sp -4
-    sw t1, 0(sp)
+    la t0, h
+    lw a7, 0(t0)
+    # A partir daqui usa a pilha para os parâmetros
     # Armazena char i = 9
-    la t0, a
+    la t0, i
     lb t1, 0(t0)
     addi sp, sp -4
     sw t1, 0(sp)
     # Armazena j = -10
-    la t0, a
+    la t0, j
     lb t1, 0(t0)
     addi sp, sp -4
     sw t1, 0(sp)
     # Armazena short k = 11
-    la t0, a
+    la t0, k
     lh t1, 0(t0)
     addi sp, sp -4
     sw t1, 0(sp)
     # Armazena short l = -12
-    la t0, a
+    la t0, l
     lh t1, 0(t0)
     addi sp, sp -4
     sw t1, 0(sp)
     # Armazena int m = 13
-    la t0, a
+    la t0, m
     lw t1, 0(t0)
     addi sp, sp -4
     sw t1, 0(sp)
     # Armazena n = -14
-    la t0, a
+    la t0, n
     lw t1, 0(t0)
     addi sp, sp -4
     sw t1, 0(sp)
+    # Armazena o ra antes de chamar outra função
+    addi sp, sp, -4
+    sw ra, 0(sp)
 
+    jal mystery_function
+    # Recupera o valor de ra e o desempilha, além de desempilhar todos os outros valores que não serão mais usados
+    lw ra, 0(sp)
+    addi sp, sp, 28
+    # Como o a0 já armazenou o retorno da mystery_function basta retornar para onde a função operation foi chamada
     ret
