5e132ba88101e72742a69db35c38433140af18d6
Caio Lima Albuquerque <caiolima2802@gmail.com>
2025-10-22 14:35:43 -0300

    Agora sim, sou incompetente, mas terminei 100%

--- lab11/c2_1.s
+++ lab11/c2_1.s
@@ -2,12 +2,29 @@
 .globl swap_int, swap_short, swap_char
 
 swap_int:
-    mv t0, a0
-    mv a0, a1
-    mv a1, t0
+    lw t0, (a0)
+    lw t1, (a1)
+    sw t0, (a1)
+    sw t1, (a0)
+
+    li a0, 0
     ret
 
-    
 swap_short:
+    lh t0, (a0)
+    lh t1, (a1)
+    sh t0, (a1)
+    sh t1, (a0)
+
+    li a0, 0
+    ret
 
 swap_char:
+    lb t0, (a0)
+    lb t1, (a1)
+    sb t0, (a1)
+    sb t1, (a0)
+
+    li a0, 0
+    ret
+
