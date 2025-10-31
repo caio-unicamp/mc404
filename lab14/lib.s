1d1bb38a1d3ce2f08f06e951f151d51b79effed0
Caio Lima Albuquerque <caiolima2802@gmail.com>
2025-10-31 16:39:02 -0300

    Seta os endereços de memória dos periféricos a serem utilizados

--- lab14/lib.s
+++ lab14/lib.s
@@ -1,5 +1,17 @@
 .text
-.globl _start, main
+.set base_gpt, 0xFFFF0100
+.set read_sys_time, 0x0
+.set tempo, 0x4
+.set temp_interromper, 0x8
+
+.set base_synthesizer, 0xFFFF0300
+.set start_play, 0x0
+.set intrument_ID, 0x02
+.set nota, 0x04
+.set vel_nota, 0x05
+.set dur_nota, 0x06
+
+.globl _start, main, _system_time, play_note
 
 _start: # Inicializa sp, seta interrupções e chama a função main
     
@@ -8,3 +20,4 @@ _start: # Inicializa sp, seta interrupções e chama a função main
     jal main
 
 
+play_note:
